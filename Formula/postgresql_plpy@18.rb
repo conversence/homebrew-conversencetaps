9f075cdb92adf6db311e4274cc51398e8147ad9c
Marc-Antoine Parent <maparent@acm.org>
2026-08-16 14:04:00 -0400

    18.6

--- Formula/postgresql_plpy@18.rb
+++ Formula/postgresql_plpy@18.rb
@@ -1,8 +1,8 @@
 class PostgresqlPlpyAT18 < Formula
   desc "Python3 as procedural language for Postgres"
   homepage "https://www.postgresql.org/"
-  url "https://ftp.postgresql.org/pub/source/v18.4/postgresql-18.4.tar.bz2"
-  sha256 "81a81ec695fb0c7901407defaa1d2f7973617154cf27ba74e3a7ab8e64436094"
+  url "https://ftp.postgresql.org/pub/source/v18.6/postgresql-18.6.tar.bz2"
+  sha256 "555610c24d53e4316da5b7d3fc25c279d96856d5e0e23ee308c328c5fa881d9f"
   license "PostgreSQL"
 
   livecheck do
@@ -12,17 +12,25 @@ class PostgresqlPlpyAT18 < Formula
 
   bottle do
     root_url "https://www.conversence.com/bottles"
-    sha256 cellar: :any, arm64_tahoe: "433a6429fded6cf5745e75f2e28bdf91da5c57806f7b824cd2c454a9640fec82"
+    sha256 cellar: :any, arm64_tahoe: "f0d418a4815afd9bfe7e8b96d026855d89ca1df0b6396931528f2d326408a70e"
   end
 
   keg_only :versioned_formula
 
   # https://www.postgresql.org/support/versioning/
   deprecate! date: "2030-11-14", because: :unsupported
+  disable! date: "2031-11-14", because: :unsupported
 
   depends_on "pkgconf" => :build
   depends_on "postgresql@18"
   depends_on "python@3.14"
+  uses_from_macos "bison" => :build
+  uses_from_macos "flex" => :build
+  uses_from_macos "curl"
+  uses_from_macos "libxml2"
+  uses_from_macos "libxslt"
+  uses_from_macos "openldap"
+  uses_from_macos "perl"
 
   def install
     # Modify Makefile to link macOS binaries using Cellar path. Otherwise, binaries are linked
@@ -32,12 +40,14 @@ class PostgresqlPlpyAT18 < Formula
     inreplace "src/Makefile.shlib", "-install_name '$(libdir)/", "-install_name '#{lib}/postgresql/"
 
     ENV["XML_CATALOG_FILES"] = etc/"xml/catalog"
+    ENV.runtime_cpu_detection
     ENV.delete "PKG_CONFIG_LIBDIR"
     ENV.prepend "LDFLAGS", "-L#{formula_opt_lib("openssl@3")} -L#{formula_opt_lib("readline")}"
     ENV.prepend "CPPFLAGS", "-I#{formula_opt_include("openssl@3")} -I#{formula_opt_include("readline")}"
     ENV.prepend "PYTHON", "#{HOMEBREW_PREFIX}/opt/python@3.14/bin/python3.14"
 
     # Fix 'libintl.h' file not found for extensions
+    # Update config to fix `error: could not find function 'gss_store_cred_into' required for GSSAPI`
     if OS.mac?
       ENV.prepend "LDFLAGS", "-L#{formula_opt_lib("gettext")} -L#{formula_opt_lib("krb5")}"
       ENV.prepend "CPPFLAGS", "-I#{formula_opt_include("gettext")} -I#{formula_opt_include("krb5")}"
@@ -58,6 +68,7 @@ class PostgresqlPlpyAT18 < Formula
       --with-lz4
       --with-zstd
       --with-openssl
+      --with-libcurl
       --with-pam
       --with-python
       --with-uuid=e2fs
